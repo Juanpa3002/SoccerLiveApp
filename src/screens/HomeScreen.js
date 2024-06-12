307c73c6bd1d68901a1571e5a03b39ed9ba2c602
emurill0 <20220021@ricaldone.edu.sv>
2024-06-12 10:00:30 -0600

    first commit

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -4,6 +4,12 @@ import { View, Text, StyleSheet, Image } from 'react-native'; // Importa Image d
 const HomeScreen = () => {
   return (
     <View style={styles.container}>
+      <Image
+        source={require('../../assets/loguito-lindo.png')} // Ruta de la imagen local
+        style={styles.loguito}
+        resizeMode="contain" // Ajusta la imagen para que quepa dentro de su contenedor sin recortar
+      />
+
       <Text style={styles.title}>
         Nike Phantom GT Elite
       </Text>
@@ -11,7 +17,22 @@ const HomeScreen = () => {
         source={require('../../assets/zapas.png')} // Ruta de la imagen local
         style={styles.image}
       />
-      
+
+      <Text style={styles.title}>
+        Nike Phantom GT Elite
+      </Text>
+      <Image
+        source={require('../../assets/zapas.png')} // Ruta de la imagen local
+        style={styles.image}
+      />
+
+     <Text style={styles.title}>
+        Nike Phantom GT Elite
+      </Text>
+      <Image
+        source={require('../../assets/zapas.png')} // Ruta de la imagen local
+        style={styles.image}
+      />
     </View>
   );
 }
@@ -20,14 +41,21 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#0A305E',
-    paddingTop: 20,
+    paddingTop: 15,
     paddingHorizontal: 15
+  },
+  loguito: {
+    width: 200, // Ancho de la imagen en píxeles
+    height: 75, // Alto de la imagen en píxeles
+    marginTop: 10, // Ajusta el margen superior según sea necesario
+    marginLeft: 10, // Ajusta el margen izquierdo
+    alignSelf: 'flex-start', // Alinea la imagen a la izquierda
   },
   title: {
     fontSize: 20,
     fontWeight: 'bold',
     textAlign: 'center',
-    marginTop: 10,
+    marginTop: 40,
     textTransform: 'uppercase',
     color: '#fff',
   },
